8d70c45c5fd9562fbb91abcd4a1499e85026f174
PolinaZherdeva <136911680+PolinaZherdeva@users.noreply.github.com>
2025-10-22 06:04:59 +0700

    game

--- game/app/src/main/java/ru/polinazherdeva/task1/game/Mouse.kt
+++
@@ -1,9 +0,0 @@
-package ru.polinazherdeva.task1.game
-
-data class Mouse(
-    // позиция
-    var x: Float = 0f,
-    var y: Float = 0f,
-    // область вщаимодействия с котом/сыром
-    var radius: Float = 80f
-)
--- game/app/src/main/java/ru/polinazherdeva/task1/level/Level.kt
+++
@@ -1,7 +0,0 @@
-package ru.polinazherdeva.task1.level
-
-data class Level(
-    val id: Int, // id уровня
-    val name: String, // название уровня
-    val difficulty: String // сложность
-)
